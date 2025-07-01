b0c3fdabcebb870086de6ad24f20dcca8c635835
Lucasdealmeid <133092843+Lucasdealmeid@users.noreply.github.com>
2025-07-01 15:13:28 +0100

    Add files via upload

--- Js.js
+++ Js.js
@@ -15,11 +15,3 @@ tailwind.config = {
             }
         }
 
-          document.addEventListener('DOMContentLoaded', function () {
-    const menuButton = document.getElementById('menu-button');
-    const mobileMenu = document.getElementById('mobile-menu');
-
-    menuButton.addEventListener('click', function () {
-      mobileMenu.classList.toggle('hidden');
-    });
-  });
